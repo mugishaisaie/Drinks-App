6d1dfe5dca55f9be64498175eb64d87142575752
mugisha isaie <mugishaisaieog1@gmail.com>
2024-12-19 19:05:21 +0200

    edit about us

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
   <Wrapper>
   <h3>About Us</h3>
   <p>
-    Introducing "MixMaster," the ultimate party sidekick app that fetches
+    Introducing "Drinks App," the ultimate party sidekick app that fetches
     cocktails from the hilarious Cocktails DB API. With a flick of your
     finger, you'll unlock a treasure trove of enchanting drink recipes
     that'll make your taste buds dance and your friends jump with joy. Get
